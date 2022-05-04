5f4f111f8e321668f53faeeb697752883893668d
Irina Romjalis <iromjalis@gmail.com>
2022-05-04 23:02:58 +0300

    added Antoninan kuva
    styled
    auto scroll to cards
    delete video from index.html

--- js/MyHelsinki.js
+++ js/MyHelsinki.js
@@ -5,20 +5,17 @@ const body = document.querySelector("body");
 let nimi, kuvaus, osoite, kuva, genre;
 const ulRef = document.querySelector(".events-list");
 const searchTitle = document.querySelector(".search-title");
-// const locationFromApi = "https://api.hel.fi/linkedevents/v1/place/tprek:7606/";
-const video = document.querySelector(".video");
+// const video = document.querySelector(".video");
 const videoWrapper = document.querySelector(".videoWrapper");
+
 let itemLocation;
 async function naytaSivu() {
   try {
-    videoWrapper.classList.add("visually-hidden");
     const haku = document.getElementById("hakuteksti").value;
     const testi = `${haku}`.trim();
     // const testi = "Helsinki";
-    console.log("testi: ", testi);
 
-    const URL = `https://api.hel.fi/linkedevents/v1/search/?type=event&q=${testi}&page=1
-    `;
+    const URL = `https://api.hel.fi/linkedevents/v1/search/?type=event&q=${testi}`;
 
     const vastaus = await fetch(`${URL}`);
 
@@ -45,7 +42,7 @@ async function naytaSivu() {
         address_locality ? (address_locality = address_locality.fi) : "No info";
         description
           ? `<p class="events-item-category">${(description =
-              description.fi)}</p>`
+              description.fi.slice(0, 109) + "...")}</p>`
           : (description = "No info");
 
         info_url ? (info_url = info_url.fi) : " ";
@@ -55,9 +52,7 @@ async function naytaSivu() {
         email ? `<p class="events-item-email">${email}</p>` : (email = "");
         provider ? (provider = provider.fi) : (provider = "No info");
 
-        //<li class="events-item" onclick="(itemLocation = '${item.location["@id"]}');" data-bs-toggle="modal" data-bs-target="#mapsModal" style="cursor: pointer;">
         itemLocation = item.location["@id"];
-        // console.log("itemLocation: ", itemLocation);
 
         return `<li class="events-item"  title="Click to open the map" onclick="window.open('/map.html?${itemLocation}', '_blank');" style="cursor: pointer;">
             <img src=${images[0].url} />
@@ -69,6 +64,13 @@ async function naytaSivu() {
       .join(" ");
     searchTitle.textContent = `Search for "${testi}"`;
     ulRef.innerHTML = markup;
+
+    // auto scroll down
+    window.scrollTo({
+      top: 700,
+
+      behavior: "smooth",
+    });
   } catch (error) {
     console.log(error);
   }
@@ -84,4 +86,5 @@ footerBtn.addEventListener("click", () => {
   if (emailInput && emailInput.value.includes("@")) {
     alert(`Your email: ${emailInput.value} are subscribed! Thank you`);
   }
+  alert("Please write your email address clearly once again");
 });
